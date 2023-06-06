119aa30e88b41438306449a0c5c688b0ecf01e9a
Etienne Millon <me@emillon.org>
2023-06-06 17:08:52 +0200

    Add provider for France
    This is based on the `eco2mix-national-tr` dataset.

--- example/main.ml
+++ example/main.ml
@@ -1,9 +1,22 @@
+let co2_signal env =
+  match Eio.Path.(load (env#fs / ".co2-token")) with
+  | exception Eio.Io _ -> ()
+  | api_key ->
+      let co2_signal = Carbon.Co2_signal.v ~api_key env#net in
+      Carbon.Co2_signal.get_intensity ~country_code:`IN co2_signal
+      |> Eio.traceln "INDIA:@.%a" Carbon.Co2_signal.Intensity.pp
+
+let gb env =
+  let gb = Carbon.Gb.v env#net in
+  Carbon.Gb.get_intensity gb |> Eio.traceln "GB:@.%a" Carbon.Gb.Intensity.pp
+
+let fr env =
+  let fr = Carbon.Fr.v env#net in
+  Carbon.Fr.get fr |> Eio.traceln "FR:@.%d"
+
 let () =
   Eio_main.run @@ fun env ->
   Mirage_crypto_rng_eio.run (module Mirage_crypto_rng.Fortuna) env @@ fun _ ->
-  let api_key = Eio.Path.(load (env#fs / ".co2-token")) in
-  let co2_signal = Carbon.Co2_signal.v ~api_key env#net in
-  Carbon.Co2_signal.get_intensity ~country_code:`FR co2_signal
-  |> Eio.traceln "FRANCE:@.%a" Carbon.Co2_signal.Intensity.pp;
-  let gb = Carbon.Gb.v env#net in
-  Carbon.Gb.get_intensity gb |> Eio.traceln "GB:@.%a" Carbon.Gb.Intensity.pp
+  co2_signal env;
+  gb env;
+  fr env
---
+++ src/plugins/fr.ml
@@ -0,0 +1,32 @@
+type t = { net : Eio.Net.t }
+
+let v net = { net }
+let url_host = "odre.opendatasoft.com"
+let url_path = "/api/explore/v2.0/catalog/datasets/eco2mix-national-tr/records"
+let encode_value = Stringext.replace_all ~pattern:" " ~with_:"%20"
+
+let query_params =
+  [
+    ("select", "taux_co2");
+    ("where", "taux_co2 is not null");
+    ("order_by", "date_heure desc");
+    ("limit", "1");
+  ]
+
+let extract_from_response json =
+  json |> J.path [ "records" ] |> J.only
+  |> J.path [ "record"; "fields"; "taux_co2" ]
+  |> J.to_int
+
+let get { net } =
+  let query_string =
+    Printf.sprintf "%s?%s" url_path
+      (List.map
+         (fun (k, v) -> Printf.sprintf "%s=%s" k (encode_value v))
+         query_params
+      |> String.concat "&")
+  in
+  Http_client.get_json ~net
+    ~headers:(Http.Header.of_list [ ("Host", url_host) ])
+    (url_host, query_string)
+  |> extract_from_response
